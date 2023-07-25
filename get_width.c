34cd6935735b8a5e6f6ef5464f8a9e0f6c0b14d6
Cherrymaher <7alarony@gmail.com>
2023-07-25 02:01:19 -0700

    a

--- get_width.c
+++ get_width.c
@@ -5,7 +5,6 @@
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
@@ -20,7 +19,8 @@ int get_width(const char *format, int *i, va_list list)
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else
+		if (format[curr_i] == '*')
 		{
 			curr_i++;
 			width = va_arg(list, int);
